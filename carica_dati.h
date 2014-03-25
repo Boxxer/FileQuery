49071683cc9494e4f51ddb5b168c058e3f2b0e61
Marco <marco.bonaventura@outlook.com>
2014-03-25 07:28:54 +0100

    Create carica_dati.h

---
+++ carica_dati.h
@@ -0,0 +1,18 @@
+/* DataReader project - file carica_dati.h */
+
+#ifndef caricadati 
+
+#define carica_dati
+
+
+class Caricadati {
+ 
+      int i;
+      int k;
+           
+public:
+       
+void LoadFile(std::string **Tab, int *Rows);
+};
+      
+#endif // carica dati
