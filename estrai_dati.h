b0f2cb06aecc27f1e59257ad13893d38d49f6875
Marco <marco.bonaventura@outlook.com>
2014-03-25 07:29:41 +0100

    Create estrai_dati.h

---
+++ estrai_dati.h
@@ -0,0 +1,20 @@
+/* DataReader project - file estrai_dati.h */
+
+#ifndef estraidati 
+
+#define estrai_dati
+
+
+class Estraidati {
+      
+       int z;
+       int ii;
+       int C;
+       
+public:
+       
+void Ricerca(std::string *chiave, std::string **Vett, int *rows, int cols, int *Prm);
+              
+};
+      
+#endif // estrai dati
